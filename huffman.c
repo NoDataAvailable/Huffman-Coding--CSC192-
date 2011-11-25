875e103abac513990c6e4d6d78f4757572961628
Roberto Bortolussi <robot.bobert@gmail.com>
2011-11-25 02:14:15 -0500

    Initial framework (LL based)

---
+++ huffman.c
@@ -0,0 +1,49 @@
+/*************************************
+ Name:                  Huffman Coding
+ Author:            Roberto Bortolussi
+ Student Number:             998837022
+ Course:                        CSC192
+**************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node *link;
+
+struct node
+{
+    int freq;
+    char val;
+    link next;
+    link prev;
+};
+
+void initializePriorityQueue(int sizeOfQueue);
+//after this function is called, your heap based priority queue condition must be satisfied.
+//see above note for the condition
+void insertInPriorityQueue(int v);
+//after this function is called, your heap based priority queue condition must be satisfied.
+//see above note for the condition
+int getMaximumPriorityItem();
+
+link queue;
+
+main()
+{
+
+};
+
+void initializePriorityQueue(int sizeOfQueue)
+{
+
+};
+
+void insertInPriorityQueue(int v)
+{
+
+};
+
+int getMaximumPriorityItem()
+{
+
+};
